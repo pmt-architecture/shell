0bd97d86683c8865a0d7a21bb14864d205412d57
Pedro Tainha <pedrotainha@gmail.com>
2026-05-11 10:46:36 +0100

    feat(shell)!: migrate to oxlint.config.ts + bump lint-config to 3.0.1

---
+++ packages/contracts/oxlint.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'oxlint';
+
+import { baseConfig } from '@-label-/lint-config/oxlint';
+
+export default defineConfig(baseConfig);
---
+++ packages/shell-hooks/oxlint.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'oxlint';
+
+import { reactConfig } from '@-label-/lint-config/oxlint';
+
+export default defineConfig(reactConfig);
